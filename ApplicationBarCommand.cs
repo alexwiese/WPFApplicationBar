0c3d7b62cec8f6a60057408b31de509ba1c0ffd1
alexwiese <alex@wiese.com.au>
2013-04-05 18:46:14 +1030

    Added code

---
+++ ApplicationBarCommand.cs
@@ -0,0 +1,29 @@
+﻿// -------------------------------------------------------------------------------------
+//
+//
+// Assembly:    WpfApplicationBar
+// Filename:    ApplicationBarCommand.cs
+// Created:     02/04/2013 1:43:57 PM
+// Version:
+//
+// 0.0.0.1      AW  02/04/2013  - Initial release.
+//
+// -------------------------------------------------------------------------------------
+
+namespace WpfApplicationBar
+{
+    internal enum ApplicationBarCommand
+    {
+        New = 0,
+        Remove,
+        QueryPosition,
+        SetPosition,
+        GetState,
+        GetTaskBarPosition,
+        Activate,
+        GetAutoHideBar,
+        SetAutoHideBar,
+        WindowPositionChanged,
+        SetState
+    }
+}
---
+++ ApplicationBarMessage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WpfApplicationBar
+{
+    internal enum ApplicationBarMessage
+    {
+        StateChanged = 0,
+        PositionChanged,
+        FullScreenApplication,
+        WindowArranged
+    }
+}
